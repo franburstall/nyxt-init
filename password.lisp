f32ad35720cf73badbd20e5b241d4c1130554386
Fran Burstall <fran.burstall@gmail.com>
2021-05-28 11:51:24 +0100

    * password.lisp: Experimental support for credential filling

---
+++ password.lisp
@@ -0,0 +1,96 @@
+;; Rudimentary support for filling in credentials on a login page
+;; 
+;; Usage: call `fill-credentials'
+;; 
+;; Limitations:
+;; 1. Only supports unix pass right now
+;; 2. Detection of the username input element is super-primitive
+;;
+
+;; first we need to grab data from the password-interface
+(in-package :password)
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (export 'get-login))
+(defgeneric get-login (password-interface &key password-name service)
+  (:documentation "Return specific login for PASSWORD-NAME and SERVICE as string."))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (export 'get-password))
+(defgeneric get-password (password-interface &key password-name service)
+  (:documentation "Return specific password for PASSWORD-NAME and SERVICE as string."))
+
+;; implementations for password-store
+
+(defmethod get-login ((password-interface password-store-interface) &key password-name service)
+  (declare (ignore service))
+  (let ((login-line (second
+		     (str:lines
+		      (execute password-interface (list "show" password-name)
+			:output '(:string :stripped t))))))
+    (cond ((and login-line (str:starts-with? "login: " login-line))
+	   (str:substring (length "login: ") nil login-line))
+	  (t
+	   (nyxt:echo-warning "No username found for ~a." password-name)
+	   ""))))
+
+(defmethod get-password ((password-interface password-store-interface) &key password-name service)
+  (declare (ignore service))
+  (let ((password (car
+		   (str:lines
+		    (execute password-interface (list "show" password-name)
+		      :output '(:string :stripped t))))))
+    (when (str:empty? password)
+      (nyxt:echo-warning "Empty password for ~a." password-name))
+    password))
+
+;; now provide a user-level command to do the job
+(in-package :nyxt)
+
+;; utilities to insert or focus various input fields
+
+;; TODO: warn if we don't find the target element
+(define-parenscript insert-in-elt (s selector)
+  (let ((elt (nyxt/ps:qs document (ps:lisp selector))))
+    (unless (null elt) (setf (ps:chain elt value) (ps:lisp s)))))
+
+
+;; TODO: more complete heuristics for finding the username field
+(defun insert-login (s)
+  "Insert string S in first plausible username field."
+  (insert-in-elt s "input[autocomplete=\"username\"]"))
+
+(defun insert-pass (s)
+  "Insert string S in first input element of type password."
+  (insert-in-elt s "input[type=\"password\"]"))
+
+(define-parenscript focus-element (selector)
+  (let ((elt (nyxt/ps:qs document (ps:lisp selector))))
+    (unless (null elt)
+      (ps:chain elt (focus)))))
+
+;; user-level entry point
+(define-command fill-credentials (&optional (buffer (current-buffer)))
+  "Fill in login credentials in BUFFER."
+  (nyxt::password-debug-info)
+  (if (password-interface buffer)
+      (nyxt::with-password (password-interface buffer)
+	(let ((nyxt::password-name
+		(first
+		 (prompt :sources
+			 (list
+			  (make-instance 'nyxt::password-source :buffer buffer
+								:password-instance
+					 (password-interface buffer)))))))
+	  (nyxt::insert-login (password::get-login (password-interface buffer)
+						   :password-name nyxt::password-name))
+	  (nyxt::insert-pass (password::get-password (password-interface buffer)
+						     :password-name nyxt::password-name))
+	  (nyxt::focus-submit)))
+      (echo-warning "No password manager found.")))
+
+(defun focus-submit ()
+  (focus-element "input[type=\"submit\"]"))
+
+;; TODO: sort out a key-binding for this
+
