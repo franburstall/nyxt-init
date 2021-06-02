0412b325c358532bf9eff7e9fc178bdc8f5bcd4d
Fran Burstall <fran.burstall@gmail.com>
2021-06-02 19:18:53 +0100

    * slynk.lisp: update spelling of nyxt(!)

--- slynk.lisp
+++ slynk.lisp
@@ -1,8 +1,8 @@
 (define-command-global start-slynk (&optional (slynk-port *swank-port*))
   "Start a Slynk server that can be connected to, for instance, in
 Emacs via SLY.
-Warning: This allows Next to be controlled remotely, that is, to
-execute arbitrary code with the privileges of the user running Next.
+Warning: This allows nyxt to be controlled remotely, that is, to
+execute arbitrary code with the privileges of the user running nyxt.
 Make sure you understand the security risks associated with this
 before running this command."
   (slynk:create-server :port slynk-port :dont-close t)
