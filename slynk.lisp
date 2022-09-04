de4cfb89fc990b6c4027ed7b933aff9b32623013
Fran Burstall <fran.burstall@gmail.com>
2022-09-04 20:43:24 +0100

    * slynk.lisp: no need to define start-slynk

--- slynk.lisp
+++ slynk.lisp
@@ -1,12 +1,12 @@
-(define-command-global start-slynk (&optional (slynk-port *swank-port*))
-  "Start a Slynk server that can be connected to, for instance, in
-Emacs via SLY.
-Warning: This allows nyxt to be controlled remotely, that is, to
-execute arbitrary code with the privileges of the user running nyxt.
-Make sure you understand the security risks associated with this
-before running this command."
-  (slynk:create-server :port slynk-port :dont-close t)
-  (echo "Slynk server started at port ~a" slynk-port))
+;; (define-command-global start-slynk (&optional (slynk-port *swank-port*))
+;;   "Start a Slynk server that can be connected to, for instance, in
+;; Emacs via SLY.
+;; Warning: This allows nyxt to be controlled remotely, that is, to
+;; execute arbitrary code with the privileges of the user running nyxt.
+;; Make sure you understand the security risks associated with this
+;; before running this command."
+;;   (slynk:create-server :port slynk-port :dont-close t)
+;;   (echo "Slynk server started at port ~a" slynk-port))
 
 ;; Start slynk by default unless one is running already
 (unless nyxt::*run-from-repl-p* (start-slynk 4006))
